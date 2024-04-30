3ef9c6d1d32e34a921de4820fdd2aa2bd5801929
Connor MacKinnon <89928233+cdmackinnon@users.noreply.github.com>
2024-04-30 18:56:30 -0400

    Add K-Fold in DecisionTree.py

--- Algorithms/DecisionTree.py
+++ Algorithms/DecisionTree.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+import numpy as np
 
 def main():
     # Read in data after preprocessing
@@ -14,10 +16,30 @@ def main():
     model = DecisionTreeClassifier()
     model.fit(X_train, y_train)
 
-    print("Accuracy:", model.score(X_test, y_test))
+    print("TTS Accuracy:", model.score(X_test, y_test))
     print("\n")
 
     #Visualize Tree
     plt.figure(figsize=(20,10))
     plot_tree(model, filled=True, feature_names=X.columns, class_names=model.classes_)
     plt.show()
+
+    #K-fold Validation
+    print("50 Fold Cross-validation average testing score: ",np.mean(cross_validation(X,y)))
+    print("\n")
+
+
+def cross_validation(X, y):
+    test_accuracies = []
+    test_scores = []
+    model = DecisionTreeClassifier()
+    kf = KFold(n_splits=50, shuffle=True)
+    X = X.values
+    y = y.values
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        model.fit(X_train, y_train)
+        test_scores.append(model.score(X_test, y_test))
+        test_accuracies.append(np.mean(test_scores))
+    return test_accuracies
